b7820938608c6618926d8e7bca4b96e176685fc9
noah the goodra <peterpan0413@live.com>
2021-07-05 16:53:56 -0500

    add fix for source_location

--- include/SKSE/Impl/PCH.h
+++ include/SKSE/Impl/PCH.h
@@ -60,7 +60,49 @@ static_assert(
 #include <boost/stl_interfaces/iterator_interface.hpp>
 #include <spdlog/spdlog.h>
 #pragma warning(pop)
+#ifndef __cpp_consteval
+namespace std {
+        inline namespace fundamentals_v2 {
+            struct source_location
+            {
+            private:
+                using uint_least32_t = unsigned;
+            public:
 
+                // 14.1.2, source_location creation
+                static constexpr source_location
+                current(const char* __file = __builtin_FILE(),
+                        const char* __func = __builtin_FUNCTION(),
+                        int __line = __builtin_LINE(),
+                        int __col = 0) noexcept
+                {
+                    source_location __loc;
+                    __loc._M_file = __file;
+                    __loc._M_func = __func;
+                    __loc._M_line = __line;
+                    __loc._M_col = __col;
+                    return __loc;
+                }
+
+                constexpr source_location() noexcept
+                    : _M_file("unknown"), _M_func(_M_file), _M_line(0), _M_col(0)
+                { }
+
+                // 14.1.3, source_location field access
+                [[nodiscard]] constexpr uint_least32_t line() const noexcept { return _M_line; }
+                [[nodiscard]] constexpr uint_least32_t column() const noexcept { return _M_col; }
+                [[nodiscard]] constexpr const char* file_name() const noexcept { return _M_file; }
+                [[nodiscard]] constexpr const char* function_name() const noexcept { return _M_func; }
+
+            private:
+                const char* _M_file;
+                const char* _M_func;
+                uint_least32_t _M_line;
+                uint_least32_t _M_col;
+            };
+        } // namespace fundamentals_v2
+} // namespace std
+#endif
 #include "SKSE/Impl/WinAPI.h"
 
 namespace SKSE
@@ -479,7 +521,7 @@ namespace SKSE
         [[noreturn]] inline void report_and_fail(std::string_view a_msg, std::source_location a_loc = std::source_location::current())
 #endif
 		{
-			const auto body = [&]() {
+			const auto body = [&]() -> std::string {
 				constexpr std::array directories{
 					"include/"sv,
 					"src/"sv,
